396ef159367ec1aff0875822a08c9147b292458a
Natchanon Manatphaiboon <natchanon.space@gmail.com>
2026-07-28 10:10:19 +0900

    moving func for eval

--- dpvg/eval/common.py
+++ dpvg/eval/common.py
@@ -1,16 +1,51 @@
-# a class that analyze from rollout
+from tensordict.nn import TensorDictModule
 
 
 class Simulator():
-    def __init__(self):
-        pass
+    def __init__(self, env, n_envs: int, max_steps: int):
+        self.env = env
+        self.n_envs = n_envs
+        self.max_steps = max_steps
 
-    def rollout_and_save(self, n_episodes: int, path: str):
-        pass
+    def rollout_and_save(self, policy: TensorDictModule, n_episodes: int, path: str):
+        ep_collection = []
 
-    def load(self):
-        pass
+        for _ in tqdm(range(n_episodes // self.n_envs), position=0):
+            self.env.reset()
+            episodes = env.rollout(
+                max_steps=self.max_steps,
+                policy=policy,
+                break_when_any_done=False,  # prevent early termination
+            )
 
+            for i in tqdm(range(episodes.shape[0]), position=1, leave=False):
+                ep = episodes[i]
+
+                ep_done: Tensor = ep.get(("next", "done")).squeeze(-1)
+                id_done = ep_done.nonzero()
+
+                if id_done.numel() == 0:
+                    id_done = ep_max_steps - 1
+                else:
+                    id_done = id_done[0]  # select only first done ep for each batch
+
+                ep_detail = []
+                for j in range(id_done+1):
+                    ep_detail.append(get_step_detail(ep, j))
+
+                ep_collection.append(ep_collection)
+
+        self.env.close()
+
+        with open(path, "wb") as f:
+            pickle.dump(ep_collection, f)
+
+        print(f"{len(ep_collection)} episodes are saved! as {path}")
+
+    def load(self, path: str) -> list[list[dict]]:
+        with open(path, "rb") as f:
+            ep_collection = pickle.load(f)
+        return ep_collection
 
 if __name__ == "__main__":
     from pprint import pprint
@@ -20,11 +55,11 @@ if __name__ == "__main__":
     from dpvg.envs.voting_game import *
     from dpvg.algorithms.common import episode_reward_ext
     from dpvg.envs.static_policies import batch_greedy_policy, batch_gini_policy
-    
+
 
     # env config and spec checking
     n_envs = 10
-    n_episodes = 10_000
+    n_episodes = 1000
     ep_max_steps = 100
 
     env_config = SimpleDpvgConfig()
@@ -39,44 +74,10 @@ if __name__ == "__main__":
     env.check_env_specs()
     print(env)
 
-    collection = []
+    simulator = Simulator(env, n_envs=n_envs, max_steps=ep_max_steps)
 
-    for _ in tqdm(range(n_episodes // n_envs), position=0):
-
-        # gini
-        env.reset()
-        episodes = env.rollout(
-            max_steps=ep_max_steps,
-            policy=batch_greedy_policy,
-            break_when_any_done=False,  # prevent early termination
-        )
-        # print(episodes.shape)
-
-        for i in tqdm(range(episodes.shape[0]), position=1, leave=False):
-            ep = episodes[i]
-            # print(ep.shape)
-
-            ep_done: Tensor = ep.get(("next", "done")).squeeze(-1)
-            id_done = ep_done.nonzero()
-
-            if id_done.numel() == 0:
-                id_done = ep_max_steps - 1
-            else:
-                id_done = id_done[0]
-
-            # print(ep)
-
-            ep_collection = []
-
-            for j in range(id_done+1):
-                ep_collection.append(get_step_detail(ep, j))
-
-            collection.append(ep_collection)
-
-    print(len(collection))
-
-    with open("outs/greedy_10000max100_default.pkl", "wb") as f:
-        pickle.dump(collection, f)
-
-    # final call
-    env.close()
+    simulator.rollout_and_save(
+        policy=batch_gini_policy,
+        n_episodes=n_episodes,
+        path="outs/trial_greedy_default.pkl"
+    )
