8d7f19fe8a17c5d48e580a359220231fdf54534c
Natchanon Manatphaiboon <natchanon.space@gmail.com>
2026-08-16 13:45:26 +0900

    ippo

---
+++ exp/07_collect_ippo.py
@@ -0,0 +1,54 @@
+import yaml
+
+from dpvg.envs.voting_game import *
+from dpvg.algorithms.common import episode_reward_ext
+from dpvg.algorithms.mappo import Mappo
+from dpvg.eval.common import Simulator
+from configs.common import dict_to_namespace
+
+
+# config
+n_episodes = 5000
+
+frames_per_batch = 4096
+max_steps = 256
+n_envs = frames_per_batch // max_steps
+
+if __name__ == "__main__":
+    # env settings
+    env_config = SimpleDpvgConfig(
+        n_agents=5,
+        k=3,
+        l=6,
+        passive=0,
+        max_steps=max_steps
+    )
+    env = get_vectorized_sdpvg(
+        env_config=env_config,
+        n_workers=n_envs,
+        mode="p",
+        flatten_obs=True
+    )
+    env = episode_reward_ext(env)
+    env.n_agents = env_config.n_agents
+    env.check_env_specs()
+
+    # mappo setup
+    with open("configs/default_ippo.yaml", "r") as f:
+            cfg = dict_to_namespace(yaml.safe_load(f))
+    mappo = Mappo(env, cfg)
+
+    for i in range(100, 1001, 100):
+        mappo.load("logs/default_ippo", f"{i}")
+
+        # collections
+        simulator = Simulator(env, n_envs, max_steps)
+
+        # collect gini
+        simulator.rollout_and_save(
+            policy=mappo.policy,
+            n_episodes=n_episodes,
+            path=f"outs/default_ippo_{i}.pkl"
+        )
+
+        env.close()
--- exp/viz.py
+++ exp/viz.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import pickle
 import numpy as np
+from scipy import stats
 
 
 def read_collection(path: str, n_episodes: int = 5000):
@@ -13,15 +14,20 @@ def ep_len(collection: list):
     for col in collection:
         last_step_info = col[-1]["info"]
         ep.append(last_step_info["episode_length"][0])
+    # print(ep)
     return ep
 
 greedy = read_collection("outs/default_greedy.pkl")
 gini = read_collection("outs/default_gini.pkl")
 mappo = read_collection("outs/default_mappo_1000.pkl")
+ippo = read_collection("outs/default_ippo_1000.pkl")
 
+# plt.hist(ep_len(greedy), bins=64, range=(0, 256), label="greedy", a="")
+# plt.hist(ep_len(gini), bins=64, range=(0, 256), label="gini")
+# plt.hist(ep_len(mappo), bins=64, range=(0, 256), label="mappo", alpha=0.5)
+# plt.hist(ep_len(ippo), bins=64, range=(0, 256), label="ippo", alpha=0.5)
 
-plt.hist(ep_len(greedy), bins=64, range=(0, 256), label="greedy", a="")
-plt.hist(ep_len(gini), bins=64, range=(0, 256), label="gini")
-plt.hist(ep_len(mappo), bins=64, range=(0, 256), label="mappo")
-plt.legend()
-plt.show()
+print(stats.ks_2samp(ep_len(mappo), ep_len(ippo)))
+
+# plt.legend()
+# plt.show()
